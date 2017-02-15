733ae3a7a8e802f2ee2cd3b9d2e2bca70f9326e1
Will Pride <wpride@dimagi.com>
2017-02-15 16:12:48 +0200

    Add comments

--- app/src/main/java/external/commcare/org/graphtools/MainActivity.java
+++ app/src/main/java/external/commcare/org/graphtools/MainActivity.java
@@ -2,59 +2,89 @@ package external.commcare.org.graphtools;
 
 import android.database.Cursor;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+import android.support.v7.app.AppCompatActivity;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
-import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
+/**
+ * Demo activity demonstrating how to read graph data from CommCare mobile UCR's
+ * and display in a graphing library
+ */
 public class MainActivity extends AppCompatActivity {
+
+    // Mobile UCR data is always in this fixture
+    private static final String REPORT_INSTANCE_ID = "commcare:reports";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         try {
-            selectFixture("commcare:reports");
+            selectFixture(REPORT_INSTANCE_ID);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void selectFixture(String fixtureId) throws Exception {
+    private String getReportXml(String fixtureId) {
         Cursor c = this.managedQuery(Uri.parse("content://org.commcare.dalvik.fixture/fixturedb/" + fixtureId), null, null, null, null);
         if (!c.moveToFirst()) {
-            return;
+            return null;
         }
-        String reportXml = c.getString(c.getColumnIndex("content"));
+        return c.getString(c.getColumnIndex("content"));
+    }
+
+    private Document getXmlDom(String reportXml) throws Exception {
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-        Document doc = dBuilder.parse(new InputSource(new StringReader(reportXml)));
-        Element workerReport = doc.getElementById("3d7782e232135e0bc1d06233cda04642c9e67d0e");
+        return dBuilder.parse(new InputSource(new StringReader(reportXml)));
+    }
+
+    private void selectFixture(String fixtureId) throws Exception {
+
+        String reportXml = getReportXml(fixtureId);
+        Document document = getXmlDom(reportXml);
+        renderDocumentToGraph(document);
+    }
+
+    /**
+     * Render our XML document into the GraphView. We are assuming an XML structure such as:
+     *
+     *       <report id="3d7782e232135e0bc1d06233cda04642c9e67d0e">
+                <filters/>
+                <rows>
+                    <row index="0" is_total_row="False">
+                        <column id="office_559ce81b">alumni</column>
+                        <column id="total_burpees">2528</column>
+                        <column id="total_goal">2700</column>
+                    </row>
+                    <row index="1" is_total_row="False">
+                        ...
+     * @param document the parsed XML document
+     */
+    private void renderDocumentToGraph(Document document) {
+        // We've hard coded in this graph ID for the moment
+        // TODO: Add a selector for this
+        Element workerReport = document.getElementById("3d7782e232135e0bc1d06233cda04642c9e67d0e");
         NodeList rows = workerReport.getElementsByTagName("row");
+
+        // We have our list of rows. Now iterate through it and pull labels and values
         ArrayList<DataPoint> dataPoints = new ArrayList<>();
         ArrayList<String> labels = new ArrayList<>();
         int count = 1;
@@ -62,30 +92,30 @@ public class MainActivity extends AppCompatActivity {
             try {
                 Node row = rows.item(i);
                 NodeList columns = ((Element) row).getElementsByTagName("column");
-                String office = columns.item(0).getChildNodes().item(0).getNodeValue();
-                String burpeeCount = columns.item(1).getChildNodes().item(0).getNodeValue();
-                dataPoints.add(new DataPoint(count, Integer.parseInt(burpeeCount)));
-                labels.add(office);
-                count++;
+                // Hacked in domain knowledge that the label is the first column, value the second
+                String label = columns.item(0).getChildNodes().item(0).getNodeValue();
+                String value = columns.item(1).getChildNodes().item(0).getNodeValue();
+                dataPoints.add(new DataPoint(count, Integer.parseInt(value)));
+                labels.add(label);
             } catch (Exception e) {
                 System.out.println("Failed to add row " + i + " with e " + e);
             }
+            count++;
         }
+
         DataPoint[] dataPointsArray = new DataPoint[dataPoints.size()];
         dataPoints.toArray(dataPointsArray);
         String[] labelsArray = new String[labels.size()];
         labels.toArray(labelsArray);
 
+        // construct our graph using http://www.android-graphview.org/
         BarGraphSeries<DataPoint> series = new BarGraphSeries<>(dataPointsArray);
         GraphView graph = (GraphView) findViewById(R.id.graph);
         series.setSpacing(50); // 50% spacing between bars
         series.setAnimated(true);
         graph.addSeries(series);
 
-        graph.getViewport().setMinX(0);
-        graph.getViewport().setMaxX(6d);
-        graph.getViewport().setXAxisBoundsManual(true);
-
+        // setup labels
         StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
         staticLabelsFormatter.setHorizontalLabels(labelsArray);
         graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
